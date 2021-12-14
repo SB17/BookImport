930f2a0bdc30f119bd78bfc0f0863d30e2bfc8d4
SB17 <sinubabu@gmail.com>
2021-12-14 15:35:17 -0500

    Added comments describing the change.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 {
     public class Program
     {
+        /**
+         * 1. I have split the monolithic BookImport class into multiple classes each fulfilling a single
+         *    responsibility. These classes are implementation of generic interfaces.
+         *    For example, XMLBookReader implements IBookReader and provides implementation of reading Books 
+         *    from an XML file. This can be substituted for a JSONBookReader (in case we start getting Books
+         *    in JSON)
+         * 2. All the classes now implement Dependency Injection.
+         * 3. The initial implementation was devoid of any error handling. That has been rectified.
+         */
         public static void Main(string[] args)
         {
             var url = "https://www.w3schools.com/xml/books.xml";
